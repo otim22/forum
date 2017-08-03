942b9db85ee0d78156dcc5596426e88c2682b2e0
vagrant <vagrant@homestead>
2017-08-03 19:32:10 +0000

    fixes exception handling conundrum

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -22,6 +22,14 @@ class CreateThreadsTest extends TestCase
     }
 
     /** @test */
+    public function guests_cannot_see_the_create_thread_page()
+    {
+        $this->withExceptionHandling('Illuminate\Auth\AuthenticationException')
+            ->get('/threads/create')
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
     public function an_authenticated_user_can_create_new_forum_thread()
     {
         $this->signIn();
