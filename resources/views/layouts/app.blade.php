d86e5a2cc8a70ac72fccea6254f1b1ffce2f31b1
vagrant <vagrant@homestead>
2017-08-04 08:47:39 +0000

    validates errors and old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,14 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;<li><a href="/threads">All Threads</a></li>
+                        &nbsp;<li>
+                            <a href="/threads">All Threads</a>
+                        </li>
+
+                        <li>
+                            <a href="/threads/create">New Thread</a>
+                        </li>
+
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown"
                                role="button" aria-haspopup="true" aria-expanded="false">Channels <span class="caret"></span>
