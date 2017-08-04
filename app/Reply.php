a7a7df489239550fa646e67f0fa00fd18f5c57f8
vagrant <vagrant@homestead>
2017-08-04 15:20:27 +0000

    favorite replies by user

--- app/Reply.php
+++ app/Reply.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Favorite;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
@@ -11,5 +12,19 @@ class Reply extends Model
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if (! $this->favorites()->where($attributes)->exists()) {
+            return $this->favorites()->create($attributes);
+        }
     }
 }
