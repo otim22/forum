cad0ea884eb0e29897374dadbaba89844efb6345
vagrant <vagrant@homestead>
2017-08-07 15:18:38 +0000

    extracts controller queries to model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Activity;
 use App\User;
+use App\Activity;
 
 class ProfilesController extends Controller
 {
@@ -13,16 +13,5 @@ class ProfilesController extends Controller
             'profileUser' => $user,
             'activities' => Activity::feed($user)
         ]);
-    }
-
-    /**
-     * @param User $user
-     * @return mixed
-     */
-    protected function getActivity(User $user)
-    {
-        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
